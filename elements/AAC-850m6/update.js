7e51744b11626ff7ac979a79143c4b6b769d6cc3
mehrdadansr <Mehrdad.Ansr@gmail.com>
2023-03-22 02:45:00 +0330

    add selector

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,9 +1,9 @@
 function(instance, properties, context) {
     var container = document.getElementById(instance.data.divName);
 
-    if(instance.data.round === 0){
-    instance.data.qabli = properties
-    instance.data.round = 1
+    if (instance.data.round === 0) {
+        instance.data.qabli = properties
+        instance.data.round = 1
     }
     /**
      * toolbar
@@ -74,7 +74,8 @@ function(instance, properties, context) {
     * New Quill
     */
     if (instance.data.quill) {
-        instance.data.quill.update()
+        instance.data.quill.update();
+
     } else {
         /**
          * options
@@ -88,34 +89,34 @@ function(instance, properties, context) {
             theme: properties.theme
         };
         instance.data.quill = new Quill(container, options);
-        
-           /*
-    *Events Trigger
-    */
-    //on text change
-    
+
+        /*
+        *Events Trigger
+        */
+
+        //on text change
         var quill = instance.data.quill;
-    quill.on('text-change', function (delta, oldDelta, source) {
-        instance.publishState("change", JSON.stringify(delta))
-        instance.publishState("beforechange", JSON.stringify(oldDelta))
-        instance.publishState("lastcontents", JSON.stringify(quill.getContents()))
-        instance.publishState("lasttext", JSON.stringify(quill.getText()))
-        instance.publishState("lastlength", JSON.stringify(quill.getLength()))
-        instance.triggerEvent("textchange")
-    });
-    //on selection change
-    quill.on('selection-change', function (range, oldRange, source) {
-        if (range) {
-            instance.publishState("selectionindex", range.index + 1)
-            instance.publishState("selectionlength", range.length)
-            instance.triggerEvent("selectionchange")
-        }
-    });
-    //on editor change
-    quill.on('editor-change', function (delta, oldDelta, source) {
-        quill.update();
-        instance.publishState("hasfocus", quill.hasFocus());
-    });
+        quill.on('text-change', function (delta, oldDelta, source) {
+            instance.publishState("change", JSON.stringify(delta))
+            instance.publishState("beforechange", JSON.stringify(oldDelta))
+            instance.publishState("lastcontents", JSON.stringify(quill.getContents()))
+            instance.publishState("lasttext", JSON.stringify(quill.getText()))
+            instance.publishState("lastlength", JSON.stringify(quill.getLength()))
+            instance.triggerEvent("textchange")
+        });
+        //on selection change
+        quill.on('selection-change', function (range, oldRange, source) {
+            if (range) {
+                instance.publishState("selectionindex", range.index + 1)
+                instance.publishState("selectionlength", range.length)
+                instance.triggerEvent("selectionchange")
+            }
+        });
+        //on editor change
+        quill.on('editor-change', function (delta, oldDelta, source) {
+            quill.update();
+            instance.publishState("hasfocus", quill.hasFocus());
+        });
     }
 
     // Insert Initials
@@ -124,75 +125,77 @@ function(instance, properties, context) {
         makeChanges();
         instance.data.qabli = properties;
     }
-    
-    function makeChanges() {    
-    var quill = instance.data.quill;
-    if (properties.initial_content) {
-        if (properties.initial_type === "Content") {
-            var initial_content = JSON.parse(properties.initial_content)
-            quill.setContents(initial_content);
-        } else {
-            quill.setText(properties.initial_content);
+
+    function makeChanges() {
+        var quill = instance.data.quill;
+        if (properties.initial_content) {
+            if (properties.initial_type === "Content") {
+                var initial_content = JSON.parse(properties.initial_content)
+                quill.setContents(initial_content);
+            } else {
+                quill.setText(properties.initial_content);
+            }
         }
-    }
 
-    quill.root.dataset.placeholder = (properties.placeholder) ? properties.placeholder : '';
+        quill.root.dataset.placeholder = (properties.placeholder) ? properties.placeholder : '';
 
-    // Get Elements
-    var parentElement = container.parentNode
-    var qlToolbar = parentElement.querySelector('.ql-toolbar');
-    var qlContainer = parentElement.querySelector('.ql-editor');
+        // Get Elements
+        var parentElement = container.parentNode
+        var qlToolbar = parentElement.querySelector('.ql-toolbar');
+        var qlContainer = parentElement.querySelector('.ql-editor');
         var babaConti = parentElement.querySelector('.ql-container');
 
-    parentElement.style.display= "flex";
-    parentElement.style.overflow = "visible";
-	parentElement.style.flexDirection = "column";
-    
+        parentElement.style.display = "flex";
+        parentElement.style.overflow = "visible";
+        parentElement.style.flexDirection = "column";
 
-    /**
-     * Toolbar CSS
-     */
-    if (qlToolbar) {
-        qlToolbar.style.border = (properties.toolbar_border_width > 0) ? `${properties.toolbar_border_width}px solid ${properties.toolbar_border_color}` : 'none';
-        qlToolbar.style.backgroundColor = properties.toolbar_bg;
-        qlToolbar.style.padding = `${properties.toolbar_padding}px`;
-    }
 
-    /**
-     *item_hover_color
-    */
-    const buttons = parentElement.querySelectorAll('.ql-toolbar button');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].style.height = `${properties.toolbar_icon_size}px`;
-        buttons[i].style.width = `${properties.toolbar_icon_size}px`;
-    }
-	
-    var parentHeighDiff = properties.bubble.height() - parentElement.clientHeight;
-    var toolbarHeight = qlToolbar.offsetHeight + parentHeighDiff;
-    if (qlContainer) {
-        babaConti.style.border = (properties.container_border_width > 0) ? `${properties.container_border_width}px solid ${properties.container_border_color}` : 'none';
-        qlContainer.style.backgroundColor = properties.container_bg;
-        qlContainer.style.padding = `${properties.container_padding}px`;
-        qlContainer.style.color = properties.placeholder_color;
-        qlContainer.style.minHeight = `${parseInt(properties.bubble.min_height_css(), 10) - toolbarHeight}px`;
-        qlContainer.style.maxHeight = `${parseInt(properties.bubble.max_height_css(), 10) - toolbarHeight}px`;
-        if(!properties.bubble.fit_height()){
-           // console.log("not_fit")
-        qlContainer.style.height = `${parentElement.clientHeight - qlToolbar.offsetHeight}px`;
+        /**
+         * Toolbar CSS
+         */
+        if (qlToolbar) {
+            qlToolbar.style.border = (properties.toolbar_border_width > 0) ? `${properties.toolbar_border_width}px solid ${properties.toolbar_border_color}` : 'none';
+            qlToolbar.style.backgroundColor = properties.toolbar_bg;
+            qlToolbar.style.padding = `${properties.toolbar_padding}px`;
         }
-        babaConti.style.fontFamily = "inherit"
-        babaConti.style.fontSize = "inherit"
-    }
 
-    const pickerOption = parentElement.querySelectorAll('.ql-picker-options');
-    for (let i = 0; i < pickerOption.length; i++) {
-        pickerOption[i].style.backgroundColor = `${properties.toolbar_bg}px`;
-    }
-    var thisID = `#${instance.data.parentID}`;
-    parentElement.id = instance.data.parentID
-    //console.log(thisID);
-    // Apply Updated CSS Classes
-   
+        /**
+         *item_hover_color
+        */
+        const buttons = parentElement.querySelectorAll('.ql-toolbar button');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.height = `${properties.toolbar_icon_size}px`;
+            buttons[i].style.width = `${properties.toolbar_icon_size}px`;
+        }
+
+        var parentHeighDiff = properties.bubble.height() - parentElement.clientHeight;
+        var toolbarHeight = qlToolbar.offsetHeight + parentHeighDiff;
+        if (qlContainer) {
+            babaConti.style.border = (properties.container_border_width > 0) ? `${properties.container_border_width}px solid ${properties.container_border_color}` : 'none';
+            qlContainer.style.backgroundColor = properties.container_bg;
+            qlContainer.style.padding = `${properties.container_padding}px`;
+            qlContainer.style.color = properties.placeholder_color;
+            qlContainer.style.minHeight = `${parseInt(properties.bubble.min_height_css(), 10) - toolbarHeight}px`;
+            qlContainer.style.maxHeight = `${parseInt(properties.bubble.max_height_css(), 10) - toolbarHeight}px`;
+            if (!properties.bubble.fit_height()) {
+                // console.log("not_fit")
+                qlContainer.style.height = `${parentElement.clientHeight - qlToolbar.offsetHeight}px`;
+            }
+            babaConti.style.fontFamily = "inherit"
+            babaConti.style.fontSize = "inherit"
+        }
+
+        const pickerOption = parentElement.querySelectorAll('.ql-picker-options');
+        for (let i = 0; i < pickerOption.length; i++) {
+            pickerOption[i].style.backgroundColor = `${properties.toolbar_bg}px`;
+        }
+        var thisID = `#${instance.data.parentID}`;
+        parentElement.id = instance.data.parentID
+        //console.log(thisID);
+        // Apply Updated CSS Classes
+
+        var buttonElemnts = parentElement.querySelector(' .ql-snow.ql-toolbar button:hover,  .ql-snow .ql-toolbar button:hover,  .ql-snow.ql-toolbar button:focus,  .ql-snow .ql-toolbar button:focus,  .ql-snow.ql-toolbar button.ql-active,  .ql-snow .ql-toolbar button.ql-active,  .ql-snow.ql-toolbar .ql-picker-label:hover,  .ql-snow .ql-toolbar .ql-picker-label:hover,  .ql-snow.ql-toolbar .ql-picker-label.ql-active,  .ql-snow .ql-toolbar .ql-picker-label.ql-active,  .ql-snow.ql-toolbar .ql-picker-item:hover,  .ql-snow .ql-toolbar .ql-picker-item:hover,  .ql-snow.ql-toolbar .ql-picker-item.ql-selected,  .ql-snow .ql-toolbar .ql-picker-item.ql-selected');
+        console.log(buttonElemnts[0]);
         var css;
         if (properties.theme === 'snow') {
             css = `${thisID} .ql-snow.ql-toolbar button:hover, ${thisID} .ql-snow .ql-toolbar button:hover, ${thisID} .ql-snow.ql-toolbar button:focus, ${thisID} .ql-snow .ql-toolbar button:focus, ${thisID} .ql-snow.ql-toolbar button.ql-active, ${thisID} .ql-snow .ql-toolbar button.ql-active, ${thisID} .ql-snow.ql-toolbar .ql-picker-label:hover, ${thisID} .ql-snow .ql-toolbar .ql-picker-label:hover, ${thisID} .ql-snow.ql-toolbar .ql-picker-label.ql-active, ${thisID} .ql-snow .ql-toolbar .ql-picker-label.ql-active, ${thisID} .ql-snow.ql-toolbar .ql-picker-item:hover, ${thisID} .ql-snow .ql-toolbar .ql-picker-item:hover, ${thisID} .ql-snow.ql-toolbar .ql-picker-item.ql-selected, ${thisID} .ql-snow .ql-toolbar .ql-picker-item.ql-selected { color: ${properties.item_hover_color}; } ${thisID} .ql-snow.ql-toolbar button:hover .ql-fill, ${thisID} .ql-snow .ql-toolbar button:hover .ql-fill,${thisID} .ql-snow.ql-toolbar button:focus .ql-fill, ${thisID} .ql-snow .ql-toolbar button:focus .ql-fill, ${thisID} .ql-snow.ql-toolbar button.ql-active .ql-fill,${thisID} .ql-snow .ql-toolbar button.ql-active .ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-label:hover .ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-label:hover .ql-fill, ${thisID} .ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-item:hover .ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-item:hover .ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-fill,${thisID} .ql-snow.ql-toolbar button:hover .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar button:hover .ql-stroke.ql-fill,${thisID} .ql-snow.ql-toolbar button:focus .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar button:focus .ql-stroke.ql-fill,${thisID} .ql-snow.ql-toolbar button.ql-active .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar button.ql-active .ql-stroke.ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke.ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke.ql-fill,${thisID} .ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke.ql-fill,${thisID} .ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke.ql-fill, ${thisID} .ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke.ql-fill, ${thisID} .ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke.ql-fill { fill: ${properties.item_hover_color}; } ${thisID} .ql-snow.ql-toolbar button:hover .ql-stroke,.ql-snow .ql-toolbar button:hover .ql-stroke,.ql-snow.ql-toolbar button:focus .ql-stroke,.ql-snow .ql-toolbar button:focus .ql-stroke,.ql-snow.ql-toolbar button.ql-active .ql-stroke,.ql-snow .ql-toolbar button.ql-active .ql-stroke,.ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke,.ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke,.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke,.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke,.ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke,.ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke,.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke,.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke,.ql-snow.ql-toolbar button:hover .ql-stroke-miter,.ql-snow .ql-toolbar button:hover .ql-stroke-miter,.ql-snow.ql-toolbar button:focus .ql-stroke-miter,.ql-snow .ql-toolbar button:focus .ql-stroke-miter,.ql-snow.ql-toolbar button.ql-active .ql-stroke-miter,.ql-snow .ql-toolbar button.ql-active .ql-stroke-miter,.ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke-miter,.ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke-miter,.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke-miter,.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke-miter,.ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke-miter,.ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke-miter,.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke-miter,.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke-miter {  stroke: ${properties.item_hover_color};} ${thisID} .ql-snow .ql-stroke {  fill: none;  stroke: ${properties.toolbar_icon_color};  stroke-linecap: round;  stroke-linejoin: round;  stroke-width: 2;} ${thisID} .ql-snow .ql-stroke-miter {  fill: none;  stroke: ${properties.toolbar_icon_color};  stroke-miterlimit: 10;  stroke-width: 2;} ${thisID} .ql-snow .ql-fill, ${thisID} .ql-snow .ql-stroke .ql-fill {  fill: ${properties.toolbar_icon_color};} ${thisID} .ql-snow .ql-empty {  fill: none;} ${thisID} .ql-snow .ql-picker {  color: ${properties.toolbar_icon_color};  display: inline-block;  float: left;  font-size: 14px;  font-weight: 500;  height: 24px;  position: relative;  vertical-align: middle;} ${thisID} .ql-snow .ql-picker-options {  background-color: ${properties.toolbar_bg};  display: none;  min-width: 100%;  padding: 4px 8px;  position: absolute;  white-space: nowrap;} ${thisID} .ql-snow .ql-picker.ql-expanded .ql-picker-label {  color: ${properties.toolbar_bg};  z-index: 2;} ${thisID} .ql-snow .ql-picker.ql-expanded .ql-picker-label .ql-fill {  fill: ${properties.toolbar_icon_color};} ${thisID} .ql-snow .ql-picker.ql-expanded .ql-picker-label .ql-stroke {  stroke: ${properties.toolbar_border_color};} .ql-editor p, .ql-editor ol, .ql-editor ul, .ql-editor pre, .ql-editor blockquote, .ql-editor h1, .ql-editor h2, .ql-editor h3, .ql-editor h4, .ql-editor h5, .ql-editor h6 {  color: ${properties.bubble.font_color()};} ${thisID} .ql-snow .ql-editor pre.ql-syntax { color: ${properties.syntax_font_color}; background-color: ${properties.syntax_bg}; border-radius: ${properties.syntax_border_radius}px;} ${thisID} .ql-snow .ql-editor blockquote { border-left: ${properties.quote_border_width}px solid ${properties.quote_border_color}; margin-bottom: 5px; margin-top: 5px; padding-left: 16px;   background-color: ${properties.quote_bg};} .ql-editor .ql-font-serif { font-family: ${properties.font_serif};} .ql-editor .ql-font-monospace { font-family: ${properties.font_mono};}`;
@@ -211,8 +214,8 @@ function(instance, properties, context) {
         }
 
         instance.data.kardam = 1;
-    
+
     }
 
- 
+
 }
