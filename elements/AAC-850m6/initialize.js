caf5393980c8dcc22a08afad371878ce1bbbbaa0
mehrdadansr <Mehrdad.Ansr@gmail.com>
2023-03-22 14:53:08 +0330

    conditional all resource

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -9,4 +9,14 @@ function(instance, context) {
 	instance.data.round = 0;
 	instance.data.parentID = "P"+instance.data.divName;
 
+	/**
+	 * <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script src="https:////cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js"></script>
+<link rel="stylesheet" href="https:////cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css">
+<link rel="stylesheet" href="https://cdn.quilljs.com/1.3.6/quill.snow.css">
+<link rel="stylesheet" href="https://cdn.quilljs.com/1.3.6/quill.bubble.css">
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+	 */
 }
